7bba5a0527935cb7b8591c2c28360e7a52cd688f
Mateus Epifanio Linhares <83597337+MateusLinhares-Dev@users.noreply.github.com>
2024-06-05 16:10:06 -0300

    Services Soap conection GetWorkflow

---
+++ ExecuteTransformation/main.py
@@ -0,0 +1,30 @@
+import sys
+import os
+
+from dotenv import load_dotenv
+
+try:
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    # IMPORT SERVICE CLIENT ZEEP
+    from api.GetWorkflow import ServiceClient
+
+except ModuleNotFoundError:
+    raise ModuleNotFoundError.__notes__
+
+# Carregar variáveis de ambiente
+load_dotenv()
+
+# Configurações da URL e Token
+wsdl_url = os.getenv('wsdl_url')
+service_url = os.getenv('service_url')
+method_url = os.getenv('method_url')
+jwt_token = os.getenv('jwt_token')
+
+# Criar o cliente do serviço com o JWT
+client = ServiceClient(wsdl_url=wsdl_url, service_url=service_url, method_url=method_url, jwt_token=jwt_token)
+
+# ID do workflow
+workflow_id = os.getenv('workflow_id')
+response = client.call_service(workflow_id=workflow_id)
+
+print(response)
---
+++ api/GetWorkflow.py
@@ -0,0 +1,47 @@
+import zeep
+from zeep import Client
+from zeep.transports import Transport
+import requests
+
+class ServiceClient:
+
+    def __init__(self, wsdl_url, service_url, method_url, jwt_token):
+        self.wsdl_url = wsdl_url
+        self.service_url = service_url
+        self.method_url = method_url
+        self.jwt_token = jwt_token
+
+        # Inicializar o transporte com cabeçalho de autenticação
+        session = requests.Session()
+        session.headers.update({'Authorization': f'{self.jwt_token}'})
+        transport = Transport(session=session)
+
+        # Inicializar o cliente Zeep com o transporte autenticado
+        self.client = zeep.Client(wsdl=self.wsdl_url, transport=transport)
+
+        # Criar o elemento do cabeçalho
+        self.header = zeep.xsd.Element(
+            "Header",
+            zeep.xsd.ComplexType(
+                [
+                    zeep.xsd.Element(
+                        "{http://www.w3.org/2005/08/addressing}Action", zeep.xsd.String()
+                    ),
+                    zeep.xsd.Element(
+                        "{http://www.w3.org/2005/08/addressing}To", zeep.xsd.String()
+                    ),
+                ]
+            ),
+        )
+
+    def call_service(self, workflow_id):
+        # Definir o valor do cabeçalho
+        header_value = self.header(Action=self.method_url, To=self.service_url)
+
+        # Fazer a chamada ao serviço SOAP
+        response = self.client.service.getWorkflow(
+            WorkflowID=workflow_id,
+            _soapheaders=[header_value]
+        )
+        return response
+
